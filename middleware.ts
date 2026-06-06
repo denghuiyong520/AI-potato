6b2a4a0e65574b1532b65e418b1e13cfddf29c34
Andy <denghuiyong520@gmail.com>
2026-06-07 02:12:41 +0800

    debug: strip middleware to bare minimum

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,7 @@
-import createMiddleware from 'next-intl/middleware'
-import { routing } from './src/i18n/routing'
-import { NextRequest } from 'next/server'
-
-const intlMiddleware = createMiddleware(routing)
+import { NextRequest, NextResponse } from 'next/server'
 
 export default function middleware(request: NextRequest) {
-  try {
-    return intlMiddleware(request)
-  } catch (e) {
-    console.error('[middleware error]', e)
-    throw e
-  }
+  return NextResponse.next()
 }
 
 export const config = {
